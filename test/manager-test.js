15a2bf35a74de7363e0b16a5e11541fad04d42c9
daesun <yume@yumes.net>
2015-01-16 17:52:52 +0900

    added initProject function

--- test/manager-test.js
+++ test/manager-test.js
@@ -5,7 +5,23 @@ describe('manager', function() {
         it('should initialize without error', function(done) {
 
             // manager service load
-            var dummyContainer = {addListener:function(){}};
+            var dummyContainer = {
+                addListener : function(){},
+                getRouteTable : function() {
+                    return {
+                        "MANAGER": {
+                            "serviceName" : "MANAGER",
+                            "moduleName" : "lecle/managerservice",
+                            "status" : "off"
+                        },
+                        "HTTP": {
+                            "serviceName" : "HTTP",
+                            "moduleName" : "lecle/httpservice",
+                            "status" : "off"
+                        }
+                    }
+                }
+            };
 
             manager.init(dummyContainer, function(err) {
 
