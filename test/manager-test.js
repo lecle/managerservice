9fa7436285e1b890688b3d1141abdeb2a224795c
daesun <yume@yumes.net>
2015-01-20 15:08:25 +0900

    added monitor #5

--- test/manager-test.js
+++ test/manager-test.js
@@ -30,4 +30,36 @@ describe('manager', function() {
         });
     });
 
+    describe('#monitor()', function() {
+        it('should monitor without error', function(done) {
+
+            // manager service load
+            var dummyContainer = {
+                addListener : function(){},
+                broadcast : function(command, req, callback){ callback(null, {data:[{data:'test'}]})},
+                log : function(log) { console.log(log)},
+                getRouteTable : function() {
+                    return {
+                        "MANAGER": {
+                            "serviceName" : "MANAGER",
+                            "moduleName" : "lecle/managerservice",
+                            "status" : "off"
+                        },
+                        "HTTP": {
+                            "serviceName" : "HTTP",
+                            "moduleName" : "lecle/httpservice",
+                            "status" : "off"
+                        }
+                    }
+                }
+            };
+
+            manager.init(dummyContainer, function(err) {
+
+                manager.monitor();
+                done();
+            });
+        });
+    });
+
 });
