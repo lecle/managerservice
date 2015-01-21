483f0d1142fae5967f3edac716e99e19409454aa
daesun <yume@yumes.net>
2015-01-21 17:53:14 +0900

    added project init process #3

--- test/manager-test.js
+++ test/manager-test.js
@@ -1,27 +1,37 @@
 var manager = require('../lib/manager');
 
+var routeTable = {
+    "MANAGER": {
+        "serviceName" : "MANAGER",
+        "moduleName" : "lecle/managerservice",
+        "status" : "off"
+    },
+    "HTTP": {
+        "serviceName" : "HTTP",
+        "moduleName" : "lecle/httpservice",
+        "status" : "off"
+    }
+};
+
+var dummyContainer = {
+    addListener : function(){},
+    broadcast : function(command, req, callback){ callback(null, {data:[{data:'test'}]})},
+    log : function(log) { console.log(log)},
+    saveRouteTable : function(){},
+    getRouteTable : function() {
+
+        return {
+            add : function() {}
+        }
+    },
+    getRouteTableJSON : function() {
+        return routeTable;
+    }
+};
+
 describe('manager', function() {
     describe('#init()', function() {
         it('should initialize without error', function(done) {
-
-            // manager service load
-            var dummyContainer = {
-                addListener : function(){},
-                getRouteTable : function() {
-                    return {
-                        "MANAGER": {
-                            "serviceName" : "MANAGER",
-                            "moduleName" : "lecle/managerservice",
-                            "status" : "off"
-                        },
-                        "HTTP": {
-                            "serviceName" : "HTTP",
-                            "moduleName" : "lecle/httpservice",
-                            "status" : "off"
-                        }
-                    }
-                }
-            };
 
             manager.init(dummyContainer, function(err) {
 
@@ -32,27 +42,6 @@ describe('manager', function() {
 
     describe('#monitor()', function() {
         it('should monitor without error', function(done) {
-
-            // manager service load
-            var dummyContainer = {
-                addListener : function(){},
-                broadcast : function(command, req, callback){ callback(null, {data:[{data:'test'}]})},
-                log : function(log) { console.log(log)},
-                getRouteTable : function() {
-                    return {
-                        "MANAGER": {
-                            "serviceName" : "MANAGER",
-                            "moduleName" : "lecle/managerservice",
-                            "status" : "off"
-                        },
-                        "HTTP": {
-                            "serviceName" : "HTTP",
-                            "moduleName" : "lecle/httpservice",
-                            "status" : "off"
-                        }
-                    }
-                }
-            };
 
             manager.init(dummyContainer, function(err) {
 
